16bcc554de729a8ca5d82051b2f102b006079bf9
shivansh13579 <shivansh0975@gmail.com>
2024-04-10 22:30:03 +0530

    create hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
   return (
 
      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">Chai or code</h1>
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
+      <UpcomingWebinars />
+      <Instructors />
      </main>
   );
 }
